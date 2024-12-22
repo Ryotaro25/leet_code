f3808b355fdcb8cf21c1e81d0f93be92a24f28f9
Ryotaro Kurita <ryo.kuri2525@gmail.com>
2024-12-22 16:53:15 +0900

    finish

---
+++ 876.MiddleoftheLinkedList/step2.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode* middleNode(ListNode* head) {
+    auto node = head;
+    map<int, ListNode*> index_for_node;
+
+    int i = 0;
+    while (node) {
+      index_for_node[i] = node;
+      node = node->next;
+      i++;
+    }
+
+    return index_for_node[i / 2];
+  }
+};
