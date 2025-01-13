eda68aa740bfdf7db0f5287a482b3ac05097bf6d
Ryotaro Kurita <ryo.kuri2525@gmail.com>
2025-01-13 20:42:34 +0900

    finish

--- 283.MoveZeroes/step2.cpp
+++ 283.MoveZeroes/step2.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-  vector<int> replaceElements(vector<int>& arr) {
-    for (int i = 0; i < arr.size(); i++) {
-      int max_val = 0;
-      for (int j = i + 1; j < arr.size(); j++) {
-        max_val = max(max_val, arr[j]);
-      }
-      arr[i] = max_val;
-
-      if (i == arr.size() - 1) {
-        arr[i] = -1;
+  void moveZeroes(vector<int>& nums) {
+    
+    int non_zero_index = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      if (nums[i] != 0) {
+        swap(nums[non_zero_index], nums[i]);
+        non_zero_index++;
       }
     }
-
-    return arr;
   }
 };
