033126a865fe0788c38ba490aea8b6ddd6b035b7
Ryotaro Kurita <ryo.kuri2525@gmail.com>
2025-11-13 17:33:45 +0900

    finish

---
+++ 1~100/82.RemoveDuplicatesfromSortedListII/defaultdict.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node_to_cnt = defaultdict(int)
+        node = head
+        while node is not None:
+           node_to_cnt[node.val] += 1
+           node = node.next
+
+        dummy_head = ListNode()
+        dummy_node = dummy_head
+        for val, cnt in node_to_cnt.items():
+            if cnt == 1:
+                dummy_node.next = ListNode(val)
+                dummy_node = dummy_node.next
+
+        return dummy_head.next
